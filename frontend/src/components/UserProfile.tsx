0836185acbe5dbeb5d229b77dffbf1c7f2119808
khang <khangruni@gmail.com>
2021-11-11 17:04:00 +0700

    add profile page

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -1,13 +1,33 @@
-import React, { SyntheticEvent, useState } from 'react';
-import {Form, Button, Row, Col} from 'react-bootstrap'
-import { Navigate } from 'react-router-dom';
-import '../css/sign.css'
+import React, { useState } from 'react';
+import { Container, Row,Col, Form } from 'react-bootstrap';
 
 
 const UserProfile = () => {
-
+    const [email, setEmail] = useState('Khangruni@gmail.com');
+    const [name, setName] = useState('Khang Nguyen Tran');
+    
     return (
-        <div>Hello</div>                    
+        <Container>
+            <Row>
+                <Col>
+                <p>This is Image</p>
+                </Col>
+                <Col>
+                    <h1>User Profile</h1>
+                    <Form className="form">     
+                        <Form.Group controlId="formCategory1">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" defaultValue={name}/>
+                        </Form.Group>
+                        <Form.Group controlId="formCategory2">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" defaultValue={email} />
+                        </Form.Group>
+                    </Form>
+                </Col>
+
+            </Row>
+        </Container>
     )
 }
 
