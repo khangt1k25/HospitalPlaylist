5b36d469fa4e4e69e28f99417af6232f558d680e
khangt1k25 <khangruni@gmail.com>
2022-01-15 22:18:06 +0700

    fix conflict

--- frontend/src/views/Craiglist.tsx
+++ frontend/src/views/Craiglist.tsx
@@ -3,16 +3,6 @@ import { useEffect, useState } from "react";
 import { getListDoctor } from '../services/admin';
 
 export default function Craiglist() {
-    const [doctor, setDoctor] = useState();
-    useEffect(() => {
-        const getData = async() => {
-            const req = await getListDoctor();
-            setDoctor(req);
-        }
-        getData()
-    },[]);
-    
-    console.log(doctor)
     return (
         <div className='Craiglist' style={{'marginBottom': 100}}>
             <Row Speciality='Healthcare'/>
