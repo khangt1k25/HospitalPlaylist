65cc25499efcfe4b9fb407763ae19301aa43e6b5
winner31082000 <winner310800@gmail.com>
2022-01-15 20:41:14 +0700

    Merge API admin

--- frontend/src/components/Admin/components/featuredInfo/FeaturedInfo.jsx
+++ frontend/src/components/Admin/components/featuredInfo/FeaturedInfo.jsx
@@ -1,77 +1,54 @@
 import "./featuredInfo.css";
 import { getListDoctor, getListUser } from "../../../../services/admin.js";
-import { useEffect, useState } from "react";
+import { Component, useEffect, useState } from "react";
 // import { getListPatient } from "../../../../services/getAppointment";
 //import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 // import { useHistory } from "react-router-dom";
 
-export default function FeaturedInfo() {
+export default class FeaturedInfo extends Component {
 
-  //var req = getListUser();
-  // console.log(req);
-  // var count_patient = 0;
-  // count_patient = req.then(function(result) {
-  //   // here you can use the result of promiseB
-  //   // console.log(result.patient_Map);
-    
-  //   return Object.keys(result.patient_Map).length; 
-  // });
-
-  // console.log(count_patient);
-  var count_patient = 0;
-  const [user, setUser] = useState();
-  const [doctor, setDoctor] = useState();
+  constructor(props) {
+    super(props);
+  }
 
 
-  useEffect(async()=>{
-    // fetchData()
-    const req1 = await getListUser();
-    // console.log(req.patient_Map);
-    setUser(Object.keys(req1.patient_Map).length);
-    const req2 = await getListDoctor();
-    var tmp=0;
-    for (const [key, value] of Object.entries(req2)) {
-        tmp += value.length;
-    }
-    setDoctor(tmp);
-    },[]);
-  // console.log(user);
-  // console.log(doctor);
-
-  return (
-    <div className="featured">
-      <div className="featuredItem" >
-        <span className="featuredTitle">PATIENTS</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">{user}</span>
-          {/* <span className="featuredMoneyRate">
-            -11.4% 
-          </span> */}
+  render() {
+    return (
+      <div className="featured">
+        <div className="featuredItem" >
+          <span className="featuredTitle">PATIENTS</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">{this.props.user}</span>
+            {/* <span className="featuredMoneyRate">
+              -11.4% 
+            </span> */}
+          </div>
+          <span className="featuredSub">Number of patients</span>
         </div>
-        <span className="featuredSub">Number of patients</span>
-      </div>
-
-      <div className="featuredItem">
-        <span className="featuredTitle">DOCTORS</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">{doctor}</span>
-          {/* <span className="featuredMoneyRate">
-            -1.4% 
-          </span> */}
+  
+        <div className="featuredItem">
+          <span className="featuredTitle">DOCTORS</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">{this.props.doctor}</span>
+            {/* <span className="featuredMoneyRate">
+              -1.4% 
+            </span> */}
+          </div>
+          <span className="featuredSub">Number of doctors</span>
         </div>
-        <span className="featuredSub">Number of doctors</span>
-      </div>
-
-      <div className="featuredItem">
-        <span className="featuredTitle">PENDING APPOINTMENT</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">xxx</span>
-          {/* <span className="featuredMoneyRate">
-            +2.4 
-          </span> */}
+  
+        <div className="featuredItem">
+          <span className="featuredTitle">PENDING APPOINTMENT</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">xxx</span>
+            {/* <span className="featuredMoneyRate">
+              +2.4 
+            </span> */}
+          </div>
+          <span className="featuredSub">Number of pending appointments</span>
         </div>
-        <span className="featuredSub">Number of pending appointments</span>
       </div>
-    </div>
-  );
+    );
+  };
+  
 }
--- frontend/src/views/Admin/home/Home.jsx
+++ frontend/src/views/Admin/home/Home.jsx
@@ -1,40 +1,58 @@
 import Chart from "../../../components/Admin/components/chart/chart";
 import FeaturedInfo from "../../../components/Admin/components/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../../../services/dummyData";
+import { userData, userTestData } from "../../../services/dummyData";
 import WidgetSm from "../../../components/Admin/components/widgetSm/WidgetSm";
 import WidgetLg from "../../../components/Admin/components/widgetLg/WidgetLg";
 import Sidebar from "../../../components/Admin/components/sidebar/Sidebar";
 import "../../../css/App.css";
 import { useEffect, useState } from "react";
-import { getCountAppointmentByYear } from "../../../services/admin";
+import { getCountAppointmentByYear, getListUser, getListDoctor } from "../../../services/admin";
 
 export default function DashBoard() {
 
-  const [data, setData] = useState(Object);
-
+  const [data, setData] = useState();
+  const [user, setUser] = useState();
+  const [doctor, setDoctor] = useState();
+  const [userList, setUserList] = useState();
 
   useEffect(async()=>{
-    // fetchData()
+    // get count appointment by year
     const year = 2021;
     const req = await getCountAppointmentByYear({"year":year});
-    setData(req);
+    //setData(req);
+    for (const [key, value] of Object.entries(req)){
+      userData[parseInt(key)-1]["Appointments"] = value;
+    }
+    setData(userData);
+
+    // count number of patient
+    const req1 = await getListUser();
+    // console.log(req.patient_Map);
+    setUser(Object.keys(req1.patient_Map).length);
+    setUserList(Object.keys(req1.patient_Map));
+    // count number of doctor
+    const req2 = await getListDoctor();
+    var tmp=0;
+    for (const [key, value] of Object.entries(req2)) {
+        tmp += value.length;
+    }
+    setDoctor(tmp);
     },[]);
 
-  //console.log(data);
-  const month = {"1":"Jan","2": "Feb","3": "Mar","4": "Apr","5": "May","6": "Jun","7": "Jul","8": "Aug","9": "Sep","10": "Oct","11": "Nov","12": "Dec"};
-  const dataChart = [];
-  for (const [key, value] of Object.entries(data)){
-    userData[parseInt(key)-1]["Appointments"] = value*1000;
-  }
+
+
   return (
     <div className="container">
       {/* <Sidebar></Sidebar> */}
 
       <div className="home">
-        <FeaturedInfo />
+        <FeaturedInfo 
+          user={user} 
+          doctor={doctor}
+        />
         <Chart
-          data={userData}
+          data={data}
           title="Appointments"
           grid
           dataKey="Appointments"
